dbbd41ef103c22451627f69b5ac0078dea643798
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-15 14:36:52 -0600

    Added the file structure and the Schema for the User.js

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const {Schema, mongoose} =require('mongoose')
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required:true,
+        unique:true,
+        trim:true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique:true,
+        match: /^\S+@\S+\.\S+$/
+    },
+    thoughts:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought'
+        }
+],
+    friends:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+        }
+    ],
+    
+},
+{
+    toJSON:{
+        virtuals:true,
+    },
+    id:false,
+}  
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function(){
+        return this.friends.length;
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
